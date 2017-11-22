ef494ea36ab0c34f3cd5100694062e10994eb32c
NETCRAFT\nataliai <nataliai@netcraft.co.il>
2017-11-22 07:17:44 +0200

    modal window

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,6 +1,8 @@
 // Ministry of Education - Tala
 
 $(document).ready(function() {
+
+	//status indication tooltip on hover
     $('.status-wrapper').on('mouseenter', '.status-index', function(event) {
         var status = $(this).data('tooltip');
         var tooltip = $('<div class="status-index-tooltip">' + status + '<div class="status-index-arrow"></div></div>');
@@ -9,4 +11,11 @@ $(document).ready(function() {
     $('.status-wrapper').on('mouseleave', '.status-index', function(event) {
         $(".status-index-tooltip").remove();
     });
+
+    //modal side bar expand and collapse
+	$('.modal-side-item-expand').on('click', function(event) {
+		$(this).parent().find('.collapsed, .expanded').toggleClass('collapsed').toggleClass('expanded');
+		$(this).toggleClass('modal-side-item-expand modal-side-item-collapse');
+	});
+
 });
